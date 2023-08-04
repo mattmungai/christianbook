c117171c6646ab2eda3a2664882b94487d8ca7d0
Matthew Mungai <blue.samango@gmail.com>
2023-08-04 11:02:45 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+// app.js
+const express = require('express');
+const productRoutes = require('./api/routes/productRoutes');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+// Routes
+app.use('/api/products', productRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
