61bc3112a4542ad71216d521de69c5dbdd4257ed
Matthew Mungai <blue.samango@gmail.com>
2023-08-09 10:57:45 -0400

    sum

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 // Routes
-app.use('/api/products', productRoutes);
+app.use('/api/routes', productRoutes);
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
